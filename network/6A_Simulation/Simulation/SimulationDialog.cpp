d55c4c33c739938c82cc99d344c3ad43a305a566
Stone <guo152@126.com>
2017-12-26 18:49:47 +0800

    6A模拟软件界面

--- network/6A_Simulation/Simulation/SimulationDialog.cpp
+++ network/6A_Simulation/Simulation/SimulationDialog.cpp
@@ -1,5 +1,4 @@
 #include "SimulationDialog.h"
-#include <QtGui>
 
 SimulationDialog::SimulationDialog(QWidget *parent) :
     QDialog(parent)
@@ -8,6 +7,12 @@ SimulationDialog::SimulationDialog(QWidget *parent) :
     createHorizontalGroupBox();
     createGridGroupBox();
     createFormGroupBox();
+    createAudioTestGroupBox();
+    createCameraSettingGroupBox();
+    createDeviceStateGroupBox();
+    createImagePreviewGroupBox();
+    createSwitchTestGroupBox();
+    createStatusBar();
 
     bigEditor = new QTextEdit;
     bigEditor->setPlainText(tr("This widget takes up all the remaining space "
@@ -19,9 +24,16 @@ SimulationDialog::SimulationDialog(QWidget *parent) :
     connect(buttonBox, SIGNAL(accepted()), this, SLOT(accept()));
     connect(buttonBox, SIGNAL(rejected()), this, SLOT(reject()));
 
+    QVBoxLayout *statusLayout = new QVBoxLayout;
+    statusLayout->addWidget(deviceStateGroupBox);
+    statusLayout->addWidget(audioTestGroupBox);
+    statusLayout->addWidget(switchTestGroupBox);
+    statusLayout->addWidget(cameraSettingGroupBox);
+    statusLayout->addWidget(imagePreviewGroupBox);
+
     QVBoxLayout *mainLayout = new QVBoxLayout;
 
-    mainLayout->setMenuBar(menuBar);
+//    mainLayout->setMenuBar(menuBar);
 
     mainLayout->addWidget(horizontalGroupBox);
     mainLayout->addWidget(gridGroupBox);
@@ -29,34 +41,92 @@ SimulationDialog::SimulationDialog(QWidget *parent) :
     mainLayout->addWidget(bigEditor);
     mainLayout->addWidget(buttonBox);
 
-    setLayout(mainLayout);
+    QGridLayout *Layout = new QGridLayout;
+
+    Layout->setMenuBar(menuBar);
+//    Layout->setSpacing(50);
+//    statusLayout->setSpacing(25);
+//    mainLayout->setSpacing(25);
+    Layout->addLayout(statusLayout, 0, 0);
+    Layout->addLayout(mainLayout, 0, 1);
+
+
+    Layout->addWidget(labelCompany, 1, 0);
+    Layout->addWidget(labelStatus, 1,1);
+
+    setLayout(Layout);
 
     setWindowTitle(tr("Basic Layouts"));
 }
 
-
-void SimulationDialog::createMenu()
+void SimulationDialog::createAudioTestGroupBox()
 {
-    menuBar = new QMenuBar;
+    audioTestGroupBox = new QGroupBox(tr("Audio Test"));
 
-    fileMenu = new QMenu(tr("&File"), this);
-    exitAction = fileMenu->addAction(tr("E&xit"));
-    menuBar->addMenu(fileMenu);
+    QGridLayout *layout = new QGridLayout;
 
-    connect(exitAction, SIGNAL(triggered()), this, SLOT(accept()));
+    for (int i = 0; i < NumAudioCannels; ++i) {
+        buttonRecordSound[i] = new QPushButton(tr("Channel %1 Record").arg(i + 1));
+        layout->addWidget(buttonRecordSound[i], 0, i);
+    }
+    for (int i = 0; i < NumAudioCannels; ++i) {
+        buttonPlaySound[i] = new QPushButton(tr("Channel %1 Play").arg(i + 1));
+        layout->addWidget(buttonPlaySound[i], 1, i);
+    }
+    audioTestGroupBox->setLayout(layout);
+}
+
+void SimulationDialog::createCameraSettingGroupBox()
+{
+    cameraSettingGroupBox = new QGroupBox(tr("Camera Settings"));
+
+    QGridLayout *layout = new QGridLayout;
+
+    for (int i = 0; i < NumCameras; ++i) {
+        labelCameraSetting[i] = new QLabel(tr("Camera %1:").arg(i + 1));
+        layout->addWidget(labelCameraSetting[i], (i / 3), (i % 3));
+    }
+    cameraSettingGroupBox->setLayout(layout);
+}
+
+void SimulationDialog::createDeviceStateGroupBox()
+{
+    deviceStateGroupBox = new QGroupBox(tr("Device State"));
+
+    QGridLayout *layout = new QGridLayout;
+
+    for (int i = 0; i < NumAVBoards; ++i) {
+        labelAVBoardsState[i] = new QLabel(tr("AV%1 Board State").arg(i + 1));
+        labelAVBoardsStateValue[i] = new QLabel(tr("NULL"));
+        layout->addWidget(labelAVBoardsState[i], i, 0);
+        layout->addWidget(labelAVBoardsStateValue[i], i, 1);
+    }
+    labelSoftwareVersion = new QLabel(tr("Software Version"));
+    labelHardwareVersion = new QLabel(tr("Hardware Version"));
+    labelSoftwareVersionValue = new QLabel(tr("NULL"));
+    labelHardwareVersionValue = new QLabel(tr("NULL"));
+    layout->addWidget(labelSoftwareVersion, 0, 2);
+    layout->addWidget(labelSoftwareVersionValue, 0, 3);
+    layout->addWidget(labelHardwareVersion, 0, 4);
+    layout->addWidget(labelHardwareVersionValue, 0, 5);
+    for (int i = 0; i < NumCameras; ++i) {
+        labelCamerasState[i] = new QLabel(tr("Camera %1 State").arg(i + 1));
+        labelCamerasStateValue[i] = new QLabel(tr("NULL"));
+        layout->addWidget(labelCamerasState[i], (i / 3 + NumAVBoards), (i % 3 * 2));
+        layout->addWidget(labelCamerasStateValue[i], (i / 3 + NumAVBoards), (i % 3 * 2 + 1));
+    }
+    deviceStateGroupBox->setLayout(layout);
 }
 
 
-void SimulationDialog::createHorizontalGroupBox()
+void SimulationDialog::createFormGroupBox()
 {
-    horizontalGroupBox = new QGroupBox(tr("Horizontal layout"));
-    QHBoxLayout *layout = new QHBoxLayout;
-
-    for (int i = 0; i < NumButtons; ++i) {
-        buttons[i] = new QPushButton(tr("Button %1").arg(i + 1));
-        layout->addWidget(buttons[i]);
-    }
-    horizontalGroupBox->setLayout(layout);
+    formGroupBox = new QGroupBox(tr("Form layout"));
+    QFormLayout *layout = new QFormLayout;
+    layout->addRow(new QLabel(tr("Line 1:")), new QLineEdit);
+    layout->addRow(new QLabel(tr("Line 2, long text:")), new QComboBox);
+    layout->addRow(new QLabel(tr("Line 3:")), new QSpinBox);
+    formGroupBox->setLayout(layout);
 }
 
 
@@ -83,13 +153,50 @@ void SimulationDialog::createGridGroupBox()
     gridGroupBox->setLayout(layout);
 }
 
-
-void SimulationDialog::createFormGroupBox()
+void SimulationDialog::createHorizontalGroupBox()
 {
-    formGroupBox = new QGroupBox(tr("Form layout"));
-    QFormLayout *layout = new QFormLayout;
-    layout->addRow(new QLabel(tr("Line 1:")), new QLineEdit);
-    layout->addRow(new QLabel(tr("Line 2, long text:")), new QComboBox);
-    layout->addRow(new QLabel(tr("Line 3:")), new QSpinBox);
-    formGroupBox->setLayout(layout);
+    horizontalGroupBox = new QGroupBox(tr("Horizontal layout"));
+    QHBoxLayout *layout = new QHBoxLayout;
+
+    for (int i = 0; i < NumButtons; ++i) {
+        buttons[i] = new QPushButton(tr("Button %1").arg(i + 1));
+        layout->addWidget(buttons[i]);
+    }
+    horizontalGroupBox->setLayout(layout);
 }
+
+void SimulationDialog::createImagePreviewGroupBox()
+{
+    imagePreviewGroupBox = new QGroupBox(tr("Image Preview"));
+}
+
+void SimulationDialog::createMenu()
+{
+    menuBar = new QMenuBar;
+
+    fileMenu = new QMenu(tr("&File"), this);
+    exitAction = fileMenu->addAction(tr("E&xit"));
+    menuBar->addMenu(fileMenu);
+
+    connect(exitAction, SIGNAL(triggered()), this, SLOT(accept()));
+}
+
+void SimulationDialog::createStatusBar()
+{
+    labelCompany = new QLabel(tr("thinkfreely"));
+    labelStatus = new QLabel(tr("Ready!"));
+}
+
+void SimulationDialog::createSwitchTestGroupBox()
+{
+    switchTestGroupBox = new QGroupBox(tr("Switch Test"));
+    QHBoxLayout *layout = new QHBoxLayout;
+
+    buttonSwitchInTest = new QPushButton(tr("Switch In Test"));
+    buttonSwitchOutTest = new QPushButton(tr("Switch Out Test"));
+    layout->addWidget(buttonSwitchOutTest);
+    layout->addWidget(buttonSwitchInTest);
+
+    switchTestGroupBox->setLayout(layout);
+}
+
--- network/6A_Simulation/Simulation/SimulationDialog.h
+++ network/6A_Simulation/Simulation/SimulationDialog.h
@@ -2,6 +2,7 @@
 #define SIMULATIONDIALOG_H
 
 #include <QDialog>
+#include <QtGui>
 
 QT_BEGIN_NAMESPACE
 class QAction;
@@ -22,23 +23,83 @@ public:
     explicit SimulationDialog(QWidget *parent = 0);
 
 private:
-    void createMenu();
-    void createHorizontalGroupBox();
-    void createGridGroupBox();
+    void createAudioTestGroupBox();
+    void createCameraSettingGroupBox();
+    void createDeviceStateGroupBox();
     void createFormGroupBox();
+    void createGridGroupBox();
+    void createHorizontalGroupBox();
+    void createImagePreviewGroupBox();
+    void createMenu();
+    void createStatusBar();
+    void createSwitchTestGroupBox();
 
-    enum { NumGridRows = 3, NumButtons = 4 };
+    enum { NumGridRows = 3, NumAVBoards = 3, NumButtons = 4, NumAudioCannels = 4, NumCameras = 14 };
 
     QMenuBar *menuBar;
     QGroupBox *horizontalGroupBox;
     QGroupBox *gridGroupBox;
     QGroupBox *formGroupBox;
+    QGroupBox *deviceStateGroupBox;
+    QGroupBox *audioTestGroupBox;
+    QGroupBox *switchTestGroupBox;
+    QGroupBox *cameraSettingGroupBox;
+    QGroupBox *imagePreviewGroupBox;
     QTextEdit *smallEditor;
     QTextEdit *bigEditor;
     QLabel *labels[NumGridRows];
     QLineEdit *lineEdits[NumGridRows];
     QPushButton *buttons[NumButtons];
     QDialogButtonBox *buttonBox;
+    QLabel *labelAVBoardsState[NumAVBoards];
+    QLabel *labelSoftwareVersion;
+    QLabel *labelHardwareVersion;
+    QLabel *labelCamerasState[NumCameras];
+    QLabel *labelAVBoardsStateValue[NumAVBoards];
+    QLabel *labelSoftwareVersionValue;
+    QLabel *labelHardwareVersionValue;
+    QLabel *labelCamerasStateValue[NumCameras];
+    QPushButton *buttonRecordSound[NumAudioCannels];
+    QPushButton *buttonPlaySound[NumAudioCannels];
+    QPushButton *buttonSwitchInTest;
+    QPushButton *buttonSwitchOutTest;
+    QLabel *labelCameraSetting[NumCameras];
+    QLabel *labelCompany;
+    QLabel *labelStatus;
+
+    QLabel *labelDataKilometerPost; //公里标
+    QLineEdit *lineEditDataKilometerPost;
+    QLabel *labelDataSpeed; //速度
+    QLineEdit *lineEditDataSpeed;
+    QLabel *labelDataLengthCounting;  //计长
+    QLineEdit *lineEditDataLengthCounting ;
+    QLabel *labelDataNumberOfVehicles;  //辆数
+    QLineEdit *lineEditDataNumberOfVehicles;
+    QLabel *labelDataStationNo; //车站号
+    QLineEdit *lineEditDataStationNo;
+    QLabel *labelDataDriverNumber;  //司机号
+    QLineEdit *lineEditDataDriverNumber;
+    QLabel *labelDataTrainNumber;   //车次
+    QLineEdit *lineEditDataTrainNumber1;
+    QLineEdit *lineEditDataTrainNumber2;
+    QLabel *labelDataIntersectionNumber;    //交路号
+    QLineEdit *lineEditDataIntersectionNumber;
+    QLabel *labelDataLocomotiveNumber;  //机车号
+    QLineEdit *lineEditDataLocomotiveNumber;
+    QLabel *labelDataChauffeurOccupancy;    //司机室占用
+    QComboBox *comboBoxDataChauffeurOccupancy;
+    QLabel *labelDataReconnectionInformation;    //重联信息
+    QComboBox *comboBoxDataReconnectionInformation;
+    QLabel *labelDataDeviceStatus;    //装置状态
+    QComboBox *comboBoxDataDeviceStatus;
+    QLabel *labelDataLocomotiveWorkingCondition;    //机车工况
+    QComboBox *comboBoxDataLocomotiveWorkingCondition;
+    QLabel *labelDataTheSenderID;    //发送方ID
+    QComboBox *comboBoxDataTheSenderID;
+    QLabel *labelDataTypeOfLocomotive;    //机车类型
+    QComboBox *comboBoxDataTypeOfLocomotive;
+    QComboBox *comboBoxDataShunting;    //调车、非调车
+
 
     QMenu *fileMenu;
     QAction *exitAction;
